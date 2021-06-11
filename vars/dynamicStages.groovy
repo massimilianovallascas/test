bc6d3f94ef9e5d213f0f2794ffb29284073ba6f0
Massimiliano Vallascas <massimiliano.vallascas@gmail.com>
2021-06-11 17:05:15 +0100

    jenkins test

--- vars/dynamicStages.groovy
+++ vars/dynamicStages.groovy
@@ -9,7 +9,7 @@ def call(def fileName) {
             stage(stageName) {
                 terraform.init()
                 terraform.plan()
-                terraform.apply()
+                terraform.build()
             }
         }
     }
