feb347a98d61d040e8f86ba276b26e6953c1319d
Massimiliano Vallascas <massimiliano.vallascas@gmail.com>
2021-06-21 11:05:02 +0100

    jenkins test

--- vars/dynamicStages.groovy
+++ vars/dynamicStages.groovy
@@ -8,7 +8,7 @@ def call(def fileName) {
         // aws.getCredentials()
         stage(stageName) {
             script {
-                terragorm.stages()
+                terraform.stages()
             }
         }
     }
