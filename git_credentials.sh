546a833f18128f9cc38f506ba5abfbef9131fe2f
Massimiliano Vallascas <massimiliano.vallascas@gmail.com>
2021-08-18 13:37:41 +0200

    test

--- git_credentials.sh
+++ git_credentials.sh
@@ -2,6 +2,6 @@
 
 gitCredentials=$(git config --list | grep "url.https://${GIT_TOKEN}@${GIT_PREFIX}.insteadOf=https://${GIT_PREFIX}" | wc -l)
 
-if [[ "gitCredentials" == "0" ]]; then 
+if [ ${gitCredentials} -eq 0 ]; then 
     git config --global url."https://${GIT_TOKEN}@${GIT_PREFIX}".insteadOf https://${GIT_PREFIX}
 fi
